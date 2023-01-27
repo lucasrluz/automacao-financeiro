b839f177f9fe2087b5237ccd7786b44a432ed9c4
lucasrluz <lucasr.luzbr@gmail.com>
2023-01-27 13:51:47 -0300

    adicionado função para fazer download da tabela no banco novo saque

--- automacao_financeiro/novo_saque/run_novo_saque.py
+++ automacao_financeiro/novo_saque/run_novo_saque.py
@@ -1,8 +1,11 @@
 from playwright.sync_api import Page
 from novo_saque.steps.login import login
 from novo_saque.steps.set_filter import set_filter
+from novo_saque.steps.table_download_novo_saque import table_download_novo_saque
 
 def run_novo_saque(page: Page):
     login(page)
 
     set_filter(page)
+
+    table_download_novo_saque(page)
