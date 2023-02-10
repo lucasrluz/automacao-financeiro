3d7b1862e382347e72eefb6137db91b86956ae3e
lucasrluz <lucasrluz@gmail.com>
2023-02-10 11:14:37 -0300

    alteração no tipo de dados na entrada de funções do facta

--- automacao_financeiro/facta/run_facta.py
+++ automacao_financeiro/facta/run_facta.py
@@ -3,13 +3,13 @@ from .clientes_efetivados.run_clientes_efetivados import run_clientes_efetivados
 from .relatorios_movimentos_fatura.run_relatorios_movimentos_fatura import run_relatorios_movimentos_fatura
 from .login.login import login
 from time import sleep
-from .clientes_efetivados.steps.util.clientes_fetivados_and_relatorios_movimento_fatura import ClientesEfetivadosAndRelatoriosMovimentoFatura
+from .clientes_efetivados.steps.util.date import Date
 
-def run_facta(page: Page, data: ClientesEfetivadosAndRelatoriosMovimentoFatura):
+def run_facta(page: Page, data: Date):
     page.goto('http://desenv.facta.com.br/sistemaNovo/login.php')
     
     login(page)
 
-    run_clientes_efetivados(page, data['clientes_efetivados'])
+    run_clientes_efetivados(page, data)
 
-    run_relatorios_movimentos_fatura(page, data['relatorios_movimentos_fatura'])
+    run_relatorios_movimentos_fatura(page, data)
