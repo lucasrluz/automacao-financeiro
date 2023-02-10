cfbc6a66701db537e18001fda5768c578b76949b
lucasrluz <lucasrluz@gmail.com>
2023-02-10 14:34:36 -0300

    remoção de configuração do diretório de download

--- automacao_financeiro/facta/relatorios_movimentos_fatura/steps/table_download_relatorios_movimentos_fatura.py
+++ automacao_financeiro/facta/relatorios_movimentos_fatura/steps/table_download_relatorios_movimentos_fatura.py
@@ -1,16 +1,7 @@
 from playwright.sync_api import Page
 from .util.elements_identifiers_relatorios_movimentos_fatura import GERAR_EXEL_BUTTON
 from time import sleep
-import shutil
 
 def table_download_relatorios_movimentos_fatura(page: Page):
     page.wait_for_selector(GERAR_EXEL_BUTTON)
     page.click(GERAR_EXEL_BUTTON)
-
-    sleep(5)
-    shutil.rmtree('/home/lucas/Documents/relatorios')
-
-    shutil.move(
-        '/home/lucas/Projects/automacao-financeiro/relatorios',
-        '/home/lucas/Documents'
-    )
