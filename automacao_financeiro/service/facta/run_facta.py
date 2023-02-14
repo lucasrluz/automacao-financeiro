8875e22d34b696e0282adba240c3ed769da0d13e
lucasrluz <lucasrluz@gmail.com>
2023-02-14 13:29:16 -0300

    adicionado execução do passo do relatorio de antecipações

--- automacao_financeiro/service/facta/run_facta.py
+++ automacao_financeiro/service/facta/run_facta.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page
 from .clientes_efetivados.run_clientes_efetivados import run_clientes_efetivados
 from .relatorios_movimentos_fatura.run_relatorios_movimentos_fatura import run_relatorios_movimentos_fatura
+from .relatorio_de_antencipacoes.run_relatorio_de_antecipacoes import run_relatorio_de_antecipacoes
 from .login.login import login
 from time import sleep
 from .clientes_efetivados.steps.util.date import Date
@@ -10,6 +11,8 @@ def run_facta(page: Page, data: Date):
     
     login(page)
 
-    run_clientes_efetivados(page, data)
+    # run_clientes_efetivados(page, data)
 
-    run_relatorios_movimentos_fatura(page, data)
+    # run_relatorios_movimentos_fatura(page, data)
+
+    run_relatorio_de_antecipacoes(page, data)
