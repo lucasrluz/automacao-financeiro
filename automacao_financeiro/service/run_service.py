08ea373d1796b27515d310001f2665eacbd5d428
lucasrluz <lucasrluz@gmail.com>
2023-02-14 13:55:36 -0300

    alteração na manipulação de arquivos baixados

--- automacao_financeiro/service/run_service.py
+++ automacao_financeiro/service/run_service.py
@@ -4,17 +4,27 @@ from .novo_saque.run_novo_saque import run_novo_saque
 from .capital_dois.run_capital_dois import run_capital_dois
 import shutil
 from time import sleep
+import os
 
 playwright = sync_playwright().start()
 
-def run_service(data, banks: list):
-    browser = playwright.chromium.launch(headless=False, channel='chromium', downloads_path='/home/lucas/Projects/automacao-financeiro/relatorios')
+def run_service(data, view_browser: str, banks: list):
+    value = True
+
+    if view_browser == '2':
+        value = False
+    
+    browser = playwright.chromium.launch(headless=value, channel='chromium', downloads_path='/home/lucas/Projects/automacao-financeiro/relatorios')
     page = browser.new_page()
 
     date = {
         'init_date': data['init_date'],
         'end_date': data['end_date']
     }
+
+    if banks[0] == 0 and banks[1] == 0 and banks[2] == 0:
+        page.close()
+        return
 
     if banks[0] == 2:
         run_facta(page, date)
@@ -25,13 +35,17 @@ def run_service(data, banks: list):
     if banks[2] == 2:
         run_capital_dois(page, date)
 
-    sleep(8)
-    shutil.rmtree('/home/lucas/Documents/relatorios')
+    # Rename and move files
+    sleep(10)
+    files = os.listdir('./relatorios')
 
-    sleep(1)
-    shutil.move(
-        '/home/lucas/Projects/automacao-financeiro/relatorios',
-        '/home/lucas/Documents'
-    )
-    sleep(1)
+    for file in files:
+        new_name = file + '.xls'
+        os.rename('./relatorios/' + file, './relatorios/' + new_name)
+
+    files_for_move = os.listdir('./relatorios')
+
+    for file in files_for_move:
+        shutil.move('./relatorios/' + file, '/home/lucas/Documents')
+
     page.close()
