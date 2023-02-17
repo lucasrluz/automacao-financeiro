9029511e1f39f5ebfa9abe51e474a847e177b42e
lucasrluz <lucasrluz@gmail.com>
2023-02-17 13:13:10 -0300

    validações caso não venha a data para selecionar no facta

--- automacao_financeiro/main.py
+++ automacao_financeiro/main.py
@@ -3,6 +3,7 @@ from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtCore import QObject, Slot, QStandardPaths
 from automacao_financeiro.service.run_service import run_service
+from datetime import date
 
 load_dotenv()
 
@@ -15,8 +16,20 @@ class Bridge(QObject):
             'banks': banks 
         }
 
-        data['init_date'] = '05/02/2023'
-        data['end_date'] = '10/02/2023'
+        if data['init_date'] == '' or data['end_date'] == '':
+            today = str(date.today())
+
+            year = today[0:4]
+            mount = today[5:7]
+            day = today[8:10]
+
+            end_date = day + '/' + mount + '/' + year
+            data['end_date'] = end_date
+
+            new_day = int(day) - 5
+
+            init_date = str(new_day) + '/' + mount + '/' + year
+            data['init_date'] = init_date
 
         run_service(data, view_browser, banks)
 
